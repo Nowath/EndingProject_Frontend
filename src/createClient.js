2a8c86cf53158392d064ee18a12df87aabd3043e
Nowath <83205731+Nowath@users.noreply.github.com>
2024-05-24 06:18:45 +0700

    ddss

--- src/createClient.js
+++ src/createClient.js
@@ -5,5 +5,5 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
     "https://msodvxyadnyjxavnnboa.supabase.co",
-    supabaseKey
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zb2R2eHlhZG55anhhdm5uYm9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzNzk0NzksImV4cCI6MjAzMTk1NTQ3OX0.jPHyUHX71KpFRTx3HinlggltfrCGVH-Xg17gP4t8cbg"
 );
