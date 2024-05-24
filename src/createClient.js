3f8db207b35c0bf2710c0faab7fd166975649a92
Nowath <83205731+Nowath@users.noreply.github.com>
2024-05-24 21:08:00 +0700

    CC

--- src/createClient.js
+++ src/createClient.js
@@ -1,8 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
 export const supabase = createClient(
     "https://msodvxyadnyjxavnnboa.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zb2R2eHlhZG55anhhdm5uYm9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzNzk0NzksImV4cCI6MjAzMTk1NTQ3OX0.jPHyUHX71KpFRTx3HinlggltfrCGVH-Xg17gP4t8cbg"
